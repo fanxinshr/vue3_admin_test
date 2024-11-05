815934d77bd0ab85136103d011c0d84962ebaba4
FAN XIN <fanxin.hit@gmail.com>
2024-11-05 17:35:42 +0900

    init repo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <h2>App Root Vue</h2>
+  </div>
+</template>
+<script setup lang="ts" name="Name">
+import {ref,reactive} from "vue"
+
+let sum = ref(12)
+
+console.log(sum.value);
+
+</script>
+<style lang="css" scoped>
+
+</style>
